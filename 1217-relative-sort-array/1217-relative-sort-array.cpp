fad2b48369cad161860ff33ec044ab0645c45e10
Pratik Vairat <84719258+pratikvairat@users.noreply.github.com>
2024-06-11 09:23:40 +0530

    Time: 5 ms (34.15%), Space: 10.3 MB (24.9%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map<int,int> mp;
+        map<int, int> mp;
         vector<int> ans;
-        for(int i=0;i<arr1.size();i++){
+        for (int i = 0; i < arr1.size(); i++) {
             mp[arr1[i]]++;
         }
-        for(auto i:arr2){
-            for(int j=0;j<mp[i];j++){
+        for (auto i : arr2) {
+            for (int j = 0; j < mp[i]; j++) {
                 ans.push_back(i);
             }
             mp.erase(i);
         }
-        for(auto i:mp){
-            for(int j=0;j<i.second;j++){
+        for (auto i : mp) {
+            for (int j = 0; j < i.second; j++) {
                 ans.push_back(i.first);
             }
         }
         return ans;
-        
     }
 };
