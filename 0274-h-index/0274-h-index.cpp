82ceae3783b702e401b40f749da2f5c3a3100334
Pratik Vairat <84719258+pratikvairat@users.noreply.github.com>
2024-06-14 22:38:01 +0530

    Time: 3 ms (48.57%), Space: 10.6 MB (98.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size();
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (citations[i] >= n - i) {
+                ans = n - i;
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+};
