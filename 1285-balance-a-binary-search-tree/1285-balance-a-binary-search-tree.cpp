cb7aeb903ae62c9343f3fff89ec1e851d9f3bcdb
Pratik Vairat <84719258+pratikvairat@users.noreply.github.com>
2024-06-26 22:07:24 +0530

    Time: 144 ms (20.98%), Space: 62.5 MB (60.81%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.cpp
@@ -12,15 +12,15 @@
 class Solution {
 public:
     vector<int> vec;
-    TreeNode* balaceTree(const vector<int>& vec,int start,int end){
+    TreeNode* balaceTree(int start,int end){
         if(start>end){
             return nullptr;
         }
         int mid=start+(end-start)/2;
         cout<<mid<<" ";
         TreeNode* node=new TreeNode(vec[mid]);
-        node->left=balaceTree(vec,start,mid-1);
-        node->right=balaceTree(vec,mid+1,end);
+        node->left=balaceTree(start,mid-1);
+        node->right=balaceTree(mid+1,end);
         return node;
     }
     void InorderTraverse(TreeNode* root){
@@ -33,7 +33,7 @@ public:
     }
     TreeNode* balanceBST(TreeNode* root) {
         InorderTraverse(root);
-        TreeNode* ans=balaceTree(vec,0,vec.size()-1);
+        TreeNode* ans=balaceTree(0,vec.size()-1);
         return ans;
     }
 };
