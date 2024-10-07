a8f6ded70dac7f3db6ba1da5ca71482474fd485c
Pratik Vairat <84719258+pratikvairat@users.noreply.github.com>
2024-10-07 22:53:39 +0530

    Time: 8 ms (42.58%), Space: 11.7 MB (18.56%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        st.push(s[0]);
+        for (int i = 1; i < s.size(); i++) {
+            if (!st.empty()) {
+                if (st.top() == 'C' && s[i] == 'D') {
+                    st.pop();
+                } else if (st.top() == 'A' && s[i] == 'B') {
+                    st.pop();
+                } else {
+                    st.push(s[i]);
+                }
+            } else {
+                st.push(s[i]);
+            }
+        }
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+        return ans.size();
+    }
+};
