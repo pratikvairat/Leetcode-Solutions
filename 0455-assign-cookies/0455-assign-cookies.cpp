7d0cf91f1a2da4c6126561fd4f1ce5d76e9f9aaf
Pratik Vairat <84719258+pratikvairat@users.noreply.github.com>
2024-12-01 14:53:08 +0530

    Time: 8 ms (64.22%), Space: 44.8 MB (32.27%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n=g.size(),m=s.size();
-        int l=0,r=0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
+        int n=g.size(),m=s.size();
+        int l=0,r=0;
         while(l<m && r<n){
             if(g[r]<=s[l]){
                 r++;
